fa6e56ea5754390132b096f20f141afbb09f5aa5
Wendell <wendellfranco99@gmail.com>
2021-09-27 14:30:07 -0300

    [Feature Wendell - Task: Implemented GetById on Postagem Controller]

--- src/main/java/org/generation/blogPessoal/controller/PostagemController.java
+++ src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,10 +19,16 @@ public class PostagemController {
 
 	@Autowired
 	private PostagemRepository repository;
-	
-	@GetMapping
-	public ResponseEntity<List<Postagem>> GetAll(){
+
+	@GetMapping("/all")
+	public ResponseEntity<List<Postagem>> GetAll() {
 		return ResponseEntity.ok(repository.findAll());
 	}
-	
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> GetById(@PathVariable long id) {
+		return repository.findById(id)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
 }
--- src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
+++ src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
@@ -1,6 +1,7 @@
 package org.generation.blogPessoal.repository;
 
 import java.util.List;
+//import java.util.Optional;
 
 import org.generation.blogPessoal.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,12 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem,Long>{
 	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);
+	
+	
+	//public Optional<Postagem> findByEmail(String email);
+	
+	//public List<Postagem> findAllByNomeContainingIgnoreCase(String nome);
+	
+	
+	
 }
