c4fc4c5eaab6f2b6aaab88c392cbc9cd2c792b50
Wendell <wendellfranco99@gmail.com>
2021-10-07 10:09:38 -0300

    [Feature Wendell - Task: ADD usuario model]

---
+++ src/main/java/org/generation/blogPessoal/model/Usuario.java
@@ -0,0 +1,74 @@
+package org.generation.blogPessoal.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario {
+
+	private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long idUsuario;
+	private @NotBlank String nome;
+	private @Email String email;
+	private @NotBlank @Size(min = 5 , max = 100 ) String senha;
+	
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE )
+	@JsonIgnoreProperties("usuario")
+	private List<Postagem> postagens = new ArrayList<>();
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Long getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(Long idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public List<Postagem> getPostagens() {
+		return postagens;
+	}
+
+	public void setPostagens(List<Postagem> postagens) {
+		this.postagens = postagens;
+	}
+
+	
+}
+
